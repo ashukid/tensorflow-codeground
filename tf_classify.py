c3893e8fc504654ac427cedd11574bd971b9f220
ashukid <irm2015001@iiita.ac.in>
2017-12-07 19:52:24 +0530

    preformatted conflict resolved

--- tf_classify.py
+++ tf_classify.py
@@ -4,15 +4,9 @@ import numpy as np
 # variables from graph
 input_layer_node_count = 2 # Number of features
 output_layer_node_count = 4 # Number of classes
-number_of_hidden_layers = 2
+number_of_hidden_layers = 1
 hidden_layer1_node_count = 10
-hidden_layer2_node_count = 10
-hidden_layer3_node_count = 10
-hidden_layer4_node_count = 10
-hidden_layer5_node_count = 10
-hidden_layer6_node_count = 10
-hidden_layer7_node_count = 10
-hidden_layer8_node_count = 10
+
 
 def convert_into_one_hot(y_train,number_of_classes):
     onehot_y=[]
@@ -25,4 +19,70 @@ def convert_into_one_hot(y_train,number_of_classes):
 
 
 def neural_network_model(data):
-	
+
+	input_params = {'weights':tf.Variable(tf.random_normal([input_layer_node_count, hidden_layer1_node_count])),
+                      'biases':tf.Variable(tf.random_normal([hidden_layer1_node_count]))}
+
+    output_params = {'weights':tf.Variable(tf.random_normal([hidden_layer1_node_count, output_layer_node_count])),
+                      'biases':tf.Variable(tf.random_normal([output_layer_node_count]))}
+
+
+
+    input_layers = tf.add(tf.matmul(data,input_params['weights']), input_params['biases'])
+    input_layers = tf.nn.relu(input_layers)
+
+    output_layers = tf.matmul(input_layers,output_params['weights']) + output_params['biases']
+ 	return output_layers
+
+
+
+def train_neural_network(x,train_x,train_y):
+
+    prediction = neural_network_model(x)
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    
+    with tf.Session() as sess:
+
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            i=0
+            while i < len(train_x):
+                start = i
+                end = i+batch_size
+                batch_x = np.array(train_x[start:end])
+                batch_y = np.array(train_y[start:end])
+
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                              y: batch_y})
+                epoch_loss += c
+                i+=batch_size
+                
+            print('Epoch', epoch+1, 'completed out of',hm_epochs,'loss:',epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+
+
+def main():
+
+
+    # user set variables
+    batch_size = 100
+    learning_rate=0.01
+    hm_epochs = 10
+
+    """ training data """                x_train= ____                                                       
+    """ labels for training data """     y_train= ____                                      
+    """converting labels to one hot """  y_train= convert_into_one_hot(y_train,number_of_classes)
+
+    x = tf.placeholder('float', [None, number_of_features])
+    y = tf.placeholder('float', [None, number_of_classes])
+
+
+    train_neural_network(x,y,x_train,y_train,batch_size,learning_rate,hm_epochs)
+
